0b9da567a640d4fce9bca993855ece2e4ebe1eec
Michelle Muñoz dg_033040 <dg_033040@mxrgcc.mail.onmicrosoft.us>
2025-09-03 16:31:59 -0400

    Updating nav drawer

--- src/components/ComponentNavDrawer.vue
+++ src/components/ComponentNavDrawer.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="parent-container nav-drawer">
+
+
   <v-card>
     <v-layout>
       <v-navigation-drawer
@@ -10,8 +11,7 @@
       >
         <v-list>
           <v-list-item
-            prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
-            title="John Leider"
+            prepend-avatar="@/assets/images/nav-bar/nav-logo-purple.png"
           >
             <template v-slot:append>
               <v-btn
@@ -30,55 +30,91 @@
 
 <!--  v-list-item  -->        
   <div class="v-list-item v-list-item--link v-list-item--nav v-theme--light v-list-item--density-default v-list-item--one-line v-list-item--square v-list-item--variant-text vantor-nav-links" tabindex="-2" aria-selected="false">
-          <span class="v-list-item__overlay"></span>
-          <span class="v-list-item__underlay"></span>
-          <div class="v-list-item__prepend"><!---->
-            <i class="v-icon notranslate v-theme--light v-icon--size-default" aria-hidden="true" density="default">
-              <div class="nav-icon">
-                <img src="@/assets/nav-bar/nav-icon-01.png"/>
-              </div>
-            </i>
-            <div class="v-list-item__spacer"></div>
-          </div>
-          <div class="v-list-item__content" data-no-activator="">
-              <div class="v-list-item-title">Imagery</div><!----><!---->
-          </div><!---->
+    <span class="v-list-item__overlay"></span>
+    <span class="v-list-item__underlay"></span>
+    <div class="v-list-item__prepend"><!---->
+      <i class="v-icon notranslate v-theme--light v-icon--size-default" aria-hidden="true" density="default">
+        <div class="nav-icon">
+          <img src="@/assets/images/nav-bar/nav-icon-01.png"/>
+        </div>
+      </i>
+      <div class="v-list-item__spacer"></div>
+    </div>
+    <div class="v-list-item__content" data-no-activator="">
+        <div class="v-list-item-title">Imagery</div><!----><!---->
+    </div><!---->
   </div>
   <!--  v-list-item  -->        
 
   <!--  v-list-item  -->        
   <div class="v-list-item v-list-item--link v-list-item--nav v-theme--light v-list-item--density-default v-list-item--one-line v-list-item--square v-list-item--variant-text vantor-nav-links" tabindex="-2" aria-selected="false">
-          <span class="v-list-item__overlay"></span>
-          <span class="v-list-item__underlay"></span>
-          <div class="v-list-item__prepend"><!---->
-            <i class="v-icon notranslate v-theme--light v-icon--size-default" aria-hidden="true" density="default">
-              <div class="nav-icon">
-                <img src="@/assets/nav-bar/nav-icon-02.png"/>
-              </div>
-            </i>
-            <div class="v-list-item__spacer"></div>
-          </div>
-          <div class="v-list-item__content" data-no-activator="">
-              <div class="v-list-item-title">Vivid</div><!----><!---->
-          </div><!---->
+    <span class="v-list-item__overlay"></span>
+    <span class="v-list-item__underlay"></span>
+    <div class="v-list-item__prepend"><!---->
+      <i class="v-icon notranslate v-theme--light v-icon--size-default" aria-hidden="true" density="default">
+        <div class="nav-icon">
+          <img src="@/assets/images/nav-bar/nav-icon-02.png"/>
+        </div>
+      </i>
+      <div class="v-list-item__spacer"></div>
+    </div>
+    <div class="v-list-item__content" data-no-activator="">
+        <div class="v-list-item-title">Vivid</div><!----><!---->
+    </div><!---->
   </div>
   <!--  v-list-item  --> 
 
-    <!--  v-list-item  -->        
+<!--  v-list-item  -->        
   <div class="v-list-item v-list-item--link v-list-item--nav v-theme--light v-list-item--density-default v-list-item--one-line v-list-item--square v-list-item--variant-text vantor-nav-links" tabindex="-2" aria-selected="false">
-          <span class="v-list-item__overlay"></span>
-          <span class="v-list-item__underlay"></span>
-          <div class="v-list-item__prepend"><!---->
-            <i class="v-icon notranslate v-theme--light v-icon--size-default" aria-hidden="true" density="default">
-              <div class="nav-icon">
-                <img src="@/assets/nav-bar/nav-icon-03.png"/>
-              </div>
-            </i>
-            <div class="v-list-item__spacer"></div>
-          </div>
-          <div class="v-list-item__content" data-no-activator="">
-              <div class="v-list-item-title">3D</div><!----><!---->
-          </div><!---->
+    <span class="v-list-item__overlay"></span>
+    <span class="v-list-item__underlay"></span>
+    <div class="v-list-item__prepend"><!---->
+      <i class="v-icon notranslate v-theme--light v-icon--size-default" aria-hidden="true" density="default">
+        <div class="nav-icon">
+          <img src="@/assets/images/nav-bar/nav-icon-03.png"/>
+        </div>
+      </i>
+      <div class="v-list-item__spacer"></div>
+    </div>
+    <div class="v-list-item__content" data-no-activator="">
+        <div class="v-list-item-title">Areas</div><!----><!---->
+    </div><!---->
+  </div>
+  <!--  v-list-item  --> 
+
+<!--  v-list-item  -->        
+  <div class="v-list-item v-list-item--link v-list-item--nav v-theme--light v-list-item--density-default v-list-item--one-line v-list-item--square v-list-item--variant-text vantor-nav-links" tabindex="-2" aria-selected="false">
+    <span class="v-list-item__overlay"></span>
+    <span class="v-list-item__underlay"></span>
+    <div class="v-list-item__prepend"><!---->
+      <i class="v-icon notranslate v-theme--light v-icon--size-default" aria-hidden="true" density="default">
+        <div class="nav-icon">
+          <img src="@/assets/images/nav-bar/nav-icon-04.png"/>
+        </div>
+      </i>
+      <div class="v-list-item__spacer"></div>
+    </div>
+    <div class="v-list-item__content" data-no-activator="">
+        <div class="v-list-item-title">Tasking</div><!----><!---->
+    </div><!---->
+  </div>
+  <!--  v-list-item  --> 
+
+  <!--  v-list-item  -->        
+  <div class="v-list-item v-list-item--link v-list-item--nav v-theme--light v-list-item--density-default v-list-item--one-line v-list-item--square v-list-item--variant-text vantor-nav-links" tabindex="-2" aria-selected="false">
+    <span class="v-list-item__overlay"></span>
+    <span class="v-list-item__underlay"></span>
+    <div class="v-list-item__prepend"><!---->
+      <i class="v-icon notranslate v-theme--light v-icon--size-default" aria-hidden="true" density="default">
+        <div class="nav-icon">
+          <img src="@/assets/images/nav-bar/nav-icon-05.png"/>
+        </div>
+      </i>
+      <div class="v-list-item__spacer"></div>
+    </div>
+    <div class="v-list-item__content" data-no-activator="">
+        <div class="v-list-item-title">Orders</div><!----><!---->
+    </div><!---->
   </div>
   <!--  v-list-item  --> 
 
@@ -88,7 +124,7 @@
     </v-layout>
   </v-card>
 
-  </div>
+
 </template>
 
 <style>
